ed944b1072db8e61173a7ff43bd49a2ccd4c4e3c
kumackey <kumak1t09e0@gmail.com>
2022-01-29 17:10:34 +0900

    usecaseを使った書き方に変更

--- cmd/main.go
+++ cmd/main.go
@@ -1,69 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"github.com/kumackey/qiita-profile/pkg/usecase"
 )
 
-const filenameReadMe = "README.md"
-const beginLine = "<!-- begin line of qiita profile -->"
-const endLine = "<!-- end line of qiita profile -->"
-
-type line string
-
 func main() {
-	f, err := os.Open(filenameReadMe)
+	u := usecase.NewUpdateReadMe()
+	err := u.Exec()
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer f.Close()
 
-	scanner := bufio.NewScanner(f)
-	var lines []line
-	for scanner.Scan() {
-		lines = append(lines, line(scanner.Text()))
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-	}
-
-	f, err = os.Create(filenameReadMe)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-
-	lines = replaceLines(lines)
-	for _, line := range lines {
-		_, err := f.WriteString(string(line) + "\n")
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
-
-func replaceLines(lines []line) []line {
-	replacedLines := make([]line, 0, len(lines))
-	writeMode := false
-	for _, line := range lines {
-		if line == endLine {
-			replacedLines = append(replacedLines, "replaced line")
-			replacedLines = append(replacedLines, line)
-			writeMode = false
-			continue
-		}
-
-		if writeMode {
-			continue
-		}
-
-		replacedLines = append(replacedLines, line)
-
-		if line == beginLine {
-			writeMode = true
-		}
-	}
-
-	return replacedLines
+	fmt.Println("success")
 }
---
+++ pkg/usecase/update_readme.go
@@ -0,0 +1,76 @@
+package usecase
+
+import (
+	"bufio"
+	"os"
+)
+
+const filenameReadMe = "README.md"
+const beginLine = "<!-- begin line of qiita profile -->"
+const endLine = "<!-- end line of qiita profile -->"
+
+type UpdateReadMe struct{}
+
+func NewUpdateReadMe() *UpdateReadMe {
+	return &UpdateReadMe{}
+}
+
+func (u *UpdateReadMe) Exec() error {
+	f, err := os.Open(filenameReadMe)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var lines []line
+	for scanner.Scan() {
+		lines = append(lines, line(scanner.Text()))
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	f, err = os.Create(filenameReadMe)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	lines = replaceLines(lines)
+	for _, line := range lines {
+		_, err := f.WriteString(string(line) + "\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type line string
+
+func replaceLines(lines []line) []line {
+	replacedLines := make([]line, 0, len(lines))
+	writeMode := false
+	for _, line := range lines {
+		if line == endLine {
+			replacedLines = append(replacedLines, "replaced line")
+			replacedLines = append(replacedLines, line)
+			writeMode = false
+			continue
+		}
+
+		if writeMode {
+			continue
+		}
+
+		replacedLines = append(replacedLines, line)
+
+		if line == beginLine {
+			writeMode = true
+		}
+	}
+
+	return replacedLines
+}
