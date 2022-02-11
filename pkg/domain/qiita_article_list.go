4226e1d158b459815befdb6158dbc078ede4a92b
kumackey <kumak1t09e0@gmail.com>
2022-02-11 12:55:40 +0900

    qiitaの記事リストの構造体を作成

--- pkg/domain/qiita_article_list.go
+++ pkg/domain/qiita_article_list.go
@@ -1,37 +1,38 @@
 package domain
 
+import "sort"
+
 type QiitaArticleList []*qiitaArticle
 
-//
-//func (l ZennArticleList) ToProfileMarkdown() string {
-//	profileMarkdown := "\n"
-//	for _, article := range l {
-//		profileMarkdown = profileMarkdown + article.toMarkdown() + "\n"
-//	}
-//
-//	return profileMarkdown
-//}
-//
-//func (l ZennArticleList) SortByPublishedAt() ZennArticleList {
-//	sort.Slice(l, func(i, j int) bool {
-//		// 公開が遅い順
-//		return l[j].publishedAt.Unix() < l[i].publishedAt.Unix()
-//	})
-//
-//	return l
-//}
-//
-//func (l ZennArticleList) Limit(limit int) ZennArticleList {
-//	list := ZennArticleList{}
-//	count := 0
-//	for _, article := range l {
-//		if limit <= count {
-//			break
-//		}
-//
-//		list = append(list, article)
-//		count += 1
-//	}
-//
-//	return list
-//}
+func (l QiitaArticleList) ToProfileMarkdown() string {
+	profileMarkdown := "\n"
+	for _, article := range l {
+		profileMarkdown = profileMarkdown + article.toMarkdown() + "\n"
+	}
+
+	return profileMarkdown
+}
+
+func (l QiitaArticleList) SortByPublishedAt() QiitaArticleList {
+	sort.Slice(l, func(i, j int) bool {
+		// 公開が遅い順
+		return l[j].publishedAt.Unix() < l[i].publishedAt.Unix()
+	})
+
+	return l
+}
+
+func (l QiitaArticleList) Limit(limit int) QiitaArticleList {
+	list := QiitaArticleList{}
+	count := 0
+	for _, article := range l {
+		if limit <= count {
+			break
+		}
+
+		list = append(list, article)
+		count += 1
+	}
+
+	return list
+}
