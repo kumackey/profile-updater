6da8d15031fb373e37f3cf55fa9aa0aa2b8e16cd
kumackey <kumak1t09e0@gmail.com>
2022-05-22 14:28:26 +0900

    feat: implements qiita api client

---
+++ pkg/adapter/qiita_api_client.go
@@ -0,0 +1,71 @@
+package adapter
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/kumackey/profile-updater/pkg/domain"
+	"github.com/kumackey/profile-updater/pkg/usecase"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type QiitaAPIClient struct{}
+
+type qiitaAPIItem struct {
+	LikeCount int    `json:"likes_count"`
+	Title     string `json:"title"`
+	URL       string `json:"url"`
+	Tags      []struct {
+		Name string `json:"name"`
+	} `json:"tags"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func (c QiitaAPIClient) FetchArticleList(ctx context.Context, userID string) (domain.QiitaArticleList, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://qiita.com/api/v2/items?per_page=10&query=qiita+user:"+userID, http.NoBody)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		if http.StatusInternalServerError < resp.StatusCode {
+			return nil, usecase.ErrQiitaInternalServerError
+		}
+
+		if resp.StatusCode == http.StatusNotFound {
+			return nil, usecase.ErrQiitaAuthorNotFound
+		}
+
+		return nil, usecase.ErrQiitaUnknownError
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data []qiitaAPIItem
+	if err = json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+
+	list := make(domain.QiitaArticleList, 0, len(data))
+	for _ = range data {
+		//publishedAt, err := time.Parse(time.RFC3339, atom.Entries[i].Published)
+		//if err != nil {
+		//	return nil, err
+		//}
+		//
+		//list = append(list, domain.NewQiitaArticle(atom.Entries[i].Title, atom.Entries[i].URL, publishedAt))
+	}
+
+	return list, nil
+}
---
+++ pkg/adapter/qiita_api_client_test.go
@@ -0,0 +1,52 @@
+package adapter
+
+import (
+	"context"
+	"math/rand"
+	"strconv"
+	"testing"
+
+	"github.com/kumackey/profile-updater/pkg/usecase"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQiitaAPIClient_FetchArticleList(t *testing.T) {
+	tests := map[string]struct {
+		userID       string
+		articleCount int
+	}{
+		"kumackeyは10記事以上書いている": {
+			"kumackey", 10,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			qiita := QiitaAPIClient{}
+			list, err := qiita.FetchArticleList(context.Background(), test.userID)
+			assert.Nil(t, err)
+			assert.GreaterOrEqual(t, len(list), test.articleCount)
+		})
+	}
+}
+
+func TestQiitaAPIClient_FetchArticleList_Failed(t *testing.T) {
+	//nolint:gosec // ランダム文字列を作りたいだけなので無視
+	random := strconv.Itoa(rand.Intn(100000))
+
+	tests := map[string]struct {
+		userID string
+	}{
+		"適当なユーザ名ではフィードが発見できない": {
+			"unknownUser" + random,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			qiita := QiitaAPIClient{}
+			_, err := qiita.FetchArticleList(context.Background(), test.userID)
+			assert.Equal(t, usecase.ErrQiitaAuthorNotFound, err)
+		})
+	}
+}
