43e715163718a8b600af4436c78e81ccd9319b50
kumackey <kumak1t09e0@gmail.com>
2024-04-04 11:54:30 +0900

    fix

--- internal/adapter/connpass_api_client.go
+++ internal/adapter/connpass_api_client.go
@@ -9,7 +9,7 @@ import (
 
 type ConnpassAPIClient struct{}
 
-func (c ConnpassAPIClient) FetchEventList(ctx context.Context, userNickname string) ([]domain.Item, error) {
+func (c ConnpassAPIClient) FetchEventList(ctx context.Context, userNickname string) ([]domain.ConnpassEvent, error) {
 	client := connpass.NewClient()
 
 	// https://connpass.com/about/api/
@@ -23,7 +23,7 @@ func (c ConnpassAPIClient) FetchEventList(ctx context.Context, userNickname stri
 		return nil, err
 	}
 
-	list := make([]domain.Item, 0, len(response.Events))
+	list := make([]domain.ConnpassEvent, 0, len(response.Events))
 	for _, event := range response.Events {
 		list = append(list, domain.NewConnpassEvent(event.Title, event.URL, event.StartedAt))
 	}
